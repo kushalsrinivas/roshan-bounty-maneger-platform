1f8e6dd150bf627769905916d2f9b01f093393d1
kushalsrinivas <kushal.s.2005@gmail.com>
2025-01-15 12:08:40 +0530

    added some faul safe

--- src/server/api/routers/audits.ts
+++ src/server/api/routers/audits.ts
@@ -25,12 +25,17 @@ export const auditRouter = createTRPCRouter({
     }),
 
   getAllAuditors: protectedProcedure.query(async ({ ctx }) => {
-    const users = await ctx.db.query.users.findMany();
+    const users = await ctx.db.query.cretfiedAuditors.findMany();
 
     return users ?? [];
   }),
   findAuditor : protectedProcedure.input(z.object({id : z.string().min(1)})).query(async ({ctx, input}) => {
-    return await ctx.db.query.users.findFirst({where : eq(users.id, input.id)});
+    const user =  await ctx.db.query.cretfiedAuditors.findFirst({where : eq(users.id, input.id)}) ;
+    if(user){
+        return user;
+    }
+    return [];
+
   })
 
 
